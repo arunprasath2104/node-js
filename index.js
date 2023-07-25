af57f3fe1447f3f0ccbc791565c865a990777ab0
arunapi40@gmail.com <arun prasad>
2023-07-25 16:20:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const {open} = require('sqlite')
+const path = require('path')
+const sqlite3 = require('sqlite3')
+const app = express()
+
+const dbPath = path.join(__dirname,'studend.db')
+
+
+
+let db = null
+
+const initialiseServerAdDatabase = async () => {
+    try {
+        db = await open({
+            filename: dbPath,
+            driver: sqlite3.Database
+        })
+        
+        app.listen(1234,()=>console.log('success'))
+    } catch (e) {
+        console.log(e.message)
+        proccess.exit(1)
+    }
+}  
+
+initialiseServerAdDatabase()
+
+app.get('/',(request,response)=>{
+    response.send('Vetri')
+})
+
+
