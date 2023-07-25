73278679e00fb8a63bcf750172ad908590365cfa
arunapi40@gmail.com <arun prasad>
2023-07-25 16:28:33 +0530

    trial 1

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const {open} = require('sqlite')
+const { open } = require('sqlite')
 const path = require('path')
 const sqlite3 = require('sqlite3')
 const app = express()
 
-const dbPath = path.join(__dirname,'studend.db')
+const dbPath = path.join(__dirname, 'studend.db')
 
 
 
@@ -16,17 +16,17 @@ const initialiseServerAdDatabase = async () => {
             filename: dbPath,
             driver: sqlite3.Database
         })
-        
-        app.listen(1234,()=>console.log('success'))
+
+        app.listen(1234, () => console.log('success'))
     } catch (e) {
         console.log(e.message)
         proccess.exit(1)
     }
-}  
+}
 
 initialiseServerAdDatabase()
 
-app.get('/',(request,response)=>{
+app.get('/', (request, response) => {
     response.send('Vetri')
 })
 
